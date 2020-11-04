a7d7e506ad12abb5641180ea0bdc5928b26cf87f
Gabriel Yeung <gabriel.yeung719@gmail.com>
2020-11-04 01:40:53 -0800

    1. Add all other activities 2. Add buttons 3. Only Config Button functional

---
+++ app/src/main/java/c/cmpt276/childapp/FlipCoinActivity.java
@@ -0,0 +1,14 @@
+package c.cmpt276.childapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlipCoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flip_coin);
+    }
+}
--- app/src/main/java/c/cmpt276/childapp/MainActivity.java
+++ app/src/main/java/c/cmpt276/childapp/MainActivity.java
@@ -12,51 +12,30 @@ import androidx.appcompat.widget.Toolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        Button btnTestConfig = findViewById(R.id.btnTestConfigure);
-        btnTestConfig.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        initializeButtons();
+    }
+
+    void initializeButtons () {
+        Button btnTestConfig = findViewById(R.id.main_config);
+        btnTestConfig.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_config:
                 startActivity(ConfigureActivity.createIntent(MainActivity.this, -1));
-            }
-        });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
+
+
