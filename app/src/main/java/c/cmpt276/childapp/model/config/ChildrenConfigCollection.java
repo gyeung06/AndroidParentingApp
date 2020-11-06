755d1507bd8a98f3fb31caf6c45bd428c8ca6df3
samuel <ssleong@sfu.ca>
2020-11-05 22:04:03 -0800

    Move gson object to static and return a List for ChildrenConfigCollection

--- app/src/main/java/c/cmpt276/childapp/model/config/ChildrenConfigCollection.java
+++ app/src/main/java/c/cmpt276/childapp/model/config/ChildrenConfigCollection.java
@@ -10,39 +10,48 @@ import java.util.List;
  * singleton class for storing all children's config
  */
 public class ChildrenConfigCollection implements Iterable<IndividualConfig> {
-    private List<IndividualConfig> children;
     private static ChildrenConfigCollection collection = new ChildrenConfigCollection();
-    public static ChildrenConfigCollection getInstance(){
-        return collection;
-    }
-    public static ChildrenConfigCollection loadWithJSONObject(String json){
-        Gson gson = new Gson();
-        collection = gson.fromJson(json,ChildrenConfigCollection.class);
-        return collection;
-    }
-    public String getJSON(){
-        Gson gson = new Gson();
-        return gson.toJson(collection);
-    }
-    public void add(IndividualConfig config){
-        children.add(config);
-    }
-    public IndividualConfig get(int i){
-        return children.get(i);
-    }
-    public int size(){
-        return children.size();
-    }
-    private ChildrenConfigCollection(){
+    private static List<IndividualConfig> children;
+    private static Gson gson = new Gson();
+
+    private ChildrenConfigCollection() {
         children = new ArrayList<>();
     }
+
+    public static ChildrenConfigCollection getInstance() {
+        return collection;
+    }
+
+    public static ChildrenConfigCollection loadWithJSONObject(String json) {
+        collection = gson.fromJson(json, ChildrenConfigCollection.class);
+        return collection;
+    }
+
+    public String getJSON() {
+        return gson.toJson(collection);
+    }
+
+    public void add(IndividualConfig config) {
+        children.add(config);
+    }
+
+    public IndividualConfig get(int i) {
+        return children.get(i);
+    }
+
+    public int size() {
+        return children.size();
+    }
+
     public Iterator<IndividualConfig> iterator() {
         return children.iterator();
     }
-    public void delete(int i){
+
+    public void delete(int i) {
         children.remove(i);
     }
-    public IndividualConfig[] getArray() {
-        return children.toArray(new IndividualConfig[children.size()]);
+
+    public List<IndividualConfig> getArray() {
+        return children;
     }
 }
